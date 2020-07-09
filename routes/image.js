301a1d748e8cb38050ba82e64d531b4f18d67640
Vinay Krishna <vinaykrishna1989@gmail.com>
2020-07-09 15:09:03 +0530

    added rezing of image

--- routes/image.js
+++ routes/image.js
@@ -1,8 +1,14 @@
 const Router = require('koa-router')
 const router = new Router({ prefix: '/image' })
+const sharp = require('sharp')
+const multer = require('@koa/multer')
+const upload = multer()
 
-router.post('/', (ctx, next) => {
-  ctx.body = 'Hello Toddle!'
+router.post('/', upload.single('big_img'), async (ctx, next) => {
+  const resizedImage = await sharp(ctx.request.file.buffer).png().resize(50, 50).toBuffer()
+  ctx.status = 200
+  ctx.attachment('thumbnail.png')
+  ctx.body = resizedImage
 })
 
 module.exports = router
